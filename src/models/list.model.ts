d30c758213574710edec214cf3a5d371ad2820a5
amandeepmittal <amandeepmittal@live.com>
2017-06-23 17:20:54 +0530

    define list model

---
+++ src/models/list.model.ts
@@ -0,0 +1,48 @@
+import { Observable } from 'rxjs/Observable';
+
+export class ListModel {
+  list: any;
+  listObserver: any;
+
+  constructor(public title: string, public items: any[]) {
+    this.items = items;
+
+    this.list = Observable.create(observer => {
+      this.listObserver = observer;
+    });
+  }
+
+  onAddItem(item) {
+    this.items.push({
+      title: item,
+      checked: false
+    });
+    this.listObserver.next(true);
+  }
+
+  onRemoveItem(item) {
+    let index = this.items.indexOf(item);
+    if(index > -1) {
+      this.items.splice(index, 1);
+    }
+    this.listObserver.next(true);
+  }
+
+  onRenameItem(item, title) {
+    let index = this.items.indexOf(item);
+    if(index > -1) {
+      this.items[index].title = title;
+    }
+    this.listObserver.next(true);
+  }
+
+  onSetTitle(title) {
+    this.title = title;
+    this.listObserver.next(true);
+  }
+
+  onToggleItem(item) {
+    item.checked = !item.checked;
+    this.listObserver.next(true);
+  }
+}
