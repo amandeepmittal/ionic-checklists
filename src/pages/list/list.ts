cfa449cd464edba280b9e88740102d382e0ce4d0
amandeepmittal <amandeepmittal@live.com>
2017-06-23 18:38:37 +0530

    feature: add new item in a list

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,25 +1,44 @@
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Component } from '@angular/core';
+import { DataProvider } from '../../providers/data/data';
 
-/**
- * Generated class for the ListPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html',
 })
 export class ListPage {
+  list: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public dataService: DataProvider) {
+    this.list = this.navParams.get('list');
   }
 
   onUncheckAllItems() {}
-  onAddItem() {}
+  onAddItem() {
+    let alert = this.alertCtrl.create({
+      title: 'New Item?',
+      message: 'Enter here',
+      inputs: [
+        {
+          name: 'name'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Add',
+          handler: dataService => {
+            this.list.addItem(dataService.name);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
   onToggleItem(item) {}
   onRenameItem(item) {}
   onRemoveItem(item) {}
