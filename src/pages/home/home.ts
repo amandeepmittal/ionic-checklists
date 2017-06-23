f4c3e53395440f44e4421811150e920b81d9f730
amandeepmittal <amandeepmittal@live.com>
2017-06-23 17:33:43 +0530

    feature: add new list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,51 @@
+import { AlertController, NavController, Platform } from 'ionic-angular';
+
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
+import { ListModel } from "../../models/list.model";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  lists: ListModel[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public dataService: DataProvider, public alertCtrl: AlertController, public platform: Platform) {
 
   }
 
-  onAddNewList() {}
-  onViewList(list: any) {}
-  onRenameList(list: any) {}
-  onRemoveList(list: any) {}
+  onAddNewList() {
+    let alert = this.alertCtrl.create({
+      title: 'New List',
+      message: 'Enter the name of your list',
+      inputs: [
+        { name: 'name' }
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: dataService => {
+            let newList = new ListModel(dataService.name, []);
+            this.lists.push(newList);
+
+            newList.list.subscribe(update => {
+              this.onSave();
+            });
+            this.onSave();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  onViewList(list) {
+
+  }
+
+  onRenameList(list) {}
+  onRemoveList(list) {}
+  onSave() {}
 }
