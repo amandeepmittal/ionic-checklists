1117e6e4fcc3920f213fc8f7ef9d22b03da00c2f
amandeepmittal <amandeepmittal@live.com>
2017-06-26 12:49:30 +0530

    feature: add side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MenuController, NavController, Platform } from 'ionic-angular';
+
 import { HomePage } from '../pages/home/home';
-import { Platform } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
@@ -9,8 +10,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 })
 export class MyApp {
   rootPage:any = HomePage;
+  @ViewChild('nav') nav: NavController
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,6 +20,11 @@ export class MyApp {
       statusBar.backgroundColorByHexString('#1f5b93');
       splashScreen.hide();
     });
+  }
+
+  onLoad(page: any) {
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
   }
 }
 
